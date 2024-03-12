ef1130b5d9dcf8b1b70917aba873452156f07ed2
TroubleSeven <591481757@qq.com>
2024-03-12 10:32:13 +0800

    feat: theme init config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript } from '@chakra-ui/react'
+import embarkyConfig from '@/config/embarky.config'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <ColorModeScript initialColorMode={embarkyConfig.theme as any} />
         <Main />
         <NextScript />
       </body>
