2b5d6a28ee3b833d91be775ddc55c0822900eb63
Echo <echo@impa.ventures>
2024-03-14 10:39:40 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -7,14 +7,14 @@ const nextConfig = {
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-  async rewrites() {
-    return [
-      {
-        source: '/ONBOARDING_API/:path*',
-        destination: 'http://13.212.200.64:8080/:path*',
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/ONBOARDING_API/:path*',
+  //       destination: 'http://13.212.200.64:8080/:path*',
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = nextConfig
