6a389b84b2e08f1cce960f22467360e723e5f48e
jc <zyc8988@gmail.com>
2024-06-27 17:27:32 +0800

    feat: add  "@embarky/react": "^0.0.4-0"

--- config/embarky.config.js
+++ config/embarky.config.js
@@ -3,10 +3,12 @@ import { bsc, mainnet } from 'viem/chains'
 const config = {
   appId: 'be78b498915f03b98bec',
   Origin: 'https://demo.embarky.xyz',
-  theme: 'dark',
+  theme: 'light',
   defaultChain: bsc,
   supportedChains: [bsc, mainnet],
   appLogo: '', // optional, size: 165px * 34px
+  allowMethods: ['farcaster', 'twitter', 'google'],
+  allowWallets: ['metaMask', 'tomo'],
   farcaster: {
     rpcUrl: 'https://optimism-rpc.publicnode.com',
     // domain: 'embarky.com',
